d8ac892fc2baee2c36dd6ce60f88d6e9cf876f80
Kamil <kamil.uhc@op.pl>
2018-05-19 14:12:12 +0200

    add EWMA method

--- HistoricalSimulation.m
+++ HistoricalSimulation.m
@@ -2,9 +2,9 @@ data = importdata('data.txt');
 sp = data.data;
 dates = data.rowheaders;
 Returns = tick2ret(sp);
-Returns = Returns(1:2000);
+Returns = Returns(end-2000+1:end);
 dates = datetime(dates, 'InputFormat', 'yyyy-MM-dd');
-DateReturns = dates(2:2001);
+DateReturns = dates(end-2000+1:end);
 SampleSize = length(Returns);
 
 TestWindowStart      = 1501;
@@ -24,14 +24,14 @@ for t = TestWindow
     Historical99(i) = quantile(X,pVaR(2));
 end
 
-count95 = 0
-count99 = 0
+count95 = 0;
+count99 = 0;
 for i=1501:2000
    if Returns(i) > Historical95(i-1500)
-      count95 = count95 + 1 
+      count95 = count95 + 1;
    end
    if Returns(i) > Historical99(i-1500)
-      count99 = count99 + 1 
+      count99 = count99 + 1;
    end
 end
 
@@ -40,6 +40,12 @@ hold on
 plot(DateReturns(1501:end),[Historical95 Historical99])
 plot(DateReturns(1501:end), Returns(1501:2000))
 ylabel('VaR')
-xlabel('Date')
-legend({'95% Confidence Level','99% Confidence Level'},'Location','Best')
-title('VaR Estimation Using the Historical Simulation Method')
+xlabel('Data')
+legend({'95% poziom unfoœci','99% poziom unfoœci'},'Location','Best')
+title('VaR przy u¿yciu zwyk³ej metody historycznej')
+
+ReturnsTest = Returns(TestWindow);
+
+vbt = varbacktest(ReturnsTest,[Historical95 Historical99],'PortfolioID','S&P','VaRID',...
+    {'Normal95','Normal99'},'VaRLevel',[0.95 0.99]);
+summary(vbt)
