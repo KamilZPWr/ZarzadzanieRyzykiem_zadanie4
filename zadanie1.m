8f3a7acb709648c6011b80cba3c51c21b40a3dc2
patpaks <wegrzynpatrycja96@gmail.com>
2018-05-20 19:00:11 +0200

    test
    test

--- zadanie1.m
+++ zadanie1.m
@@ -1,1 +1,17 @@
-test
+data = importdata('data.txt');
+sp = data.data;
+dates = data.rowheaders;
+Returns = tick2ret(sp);
+Returns = Returns(end-2000+1:end);
+dates = datetime(dates, 'InputFormat', 'yyyy-MM-dd');
+DateReturns = dates(end-2000+1:end);
+SampleSize = length(Returns);
+
+TestWindowStart      = 1501;
+TestWindow           = TestWindowStart : SampleSize;
+EstimationWindowSize = 1500;
+
+pVaR = [0.95 0.99];
+
+Historical95 = zeros(length(TestWindow),1);
+Historical99 = zeros(length(TestWindow),1);
