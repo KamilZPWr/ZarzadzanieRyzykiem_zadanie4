407d6d4099536fecd5063f81b6651a443e8a0851
patpaks <wegrzynpatrycja96@gmail.com>
2018-05-20 21:39:44 +0200

    zadanie1
    zadanie1

--- zadanie1.m
+++ zadanie1.m
@@ -13,5 +13,24 @@ EstimationWindowSize = 1500;
 
 pVaR = [0.95 0.99];
 
-Historical95 = zeros(length(TestWindow),1);
-Historical99 = zeros(length(TestWindow),1);
+Zscore   = tinv(pVaR,1:2);
+T95 = zeros(length(TestWindow),1);
+T99 = zeros(length(TestWindow),1);
+
+
+for t = TestWindow
+    i = t - TestWindowStart + 1;
+    EstimationWindow = t-EstimationWindowSize:t-1;
+    Sigma = std(Returns(EstimationWindow));
+    T95(i) = Zscore(1)*Sigma;
+    T99(i) = Zscore(2)*Sigma;
+end
+
+figure;
+hold on
+plot(DateReturns(TestWindow),[T95 T99])
+plot(DateReturns(1501:end), Returns(1501:2000))
+xlabel('Data')
+ylabel('VaR')
+legend({'95% poziom ufnoœci ','99% poziom ufnoœci'},'Location','Best')
+title('Estymacja wartoœci zagro¿onej dla metody parametrycznej')
