0a84f5c095ab68d8fc02bc7471b1fc0d653aa482
patpaks <wegrzynpatrycja96@gmail.com>
2018-05-21 23:01:55 +0200

    test
    test

--- zadanie1.m
+++ zadanie1.m
@@ -1,3 +1,5 @@
+%Metoda parametryczna
+
 data = importdata('data.txt');
 sp = data.data;
 dates = data.rowheaders;
@@ -15,24 +17,25 @@ pVaR = [0.95 0.99];
 T95 = zeros(length(TestWindow),1);
 T99 = zeros(length(TestWindow),1);
 
+%Sprawdzenie s³usznoœci wyboru rozk³adu t-Studenta
+%Dystrybuanta
+[F,x] = ecdf(Returns); 
+plot(x,F);
+hold on
+plot(x,normcdf(x,0,0.0133))
+title('Porównanie dystrybuant')
+%Kstest
+test_cdf = makedist('tlocationscale','mu',0,'sigma',0.0133,'nu',4.0343);
+[h,p] = kstest(Returns,'CDF',test_cdf,'Alpha',0.01)
 
 for t = TestWindow
     i = t - TestWindowStart + 1;
     EstimationWindow = t-EstimationWindowSize:t-1;
     X = Returns(EstimationWindow);
-    sigma = std(X);
     parameters = mle(X,'distribution','tlocationscale');
-    %Tutaj zgodnie z tym co Wojtek pisa³ i tutaj te¿ to jest: http://www.mathworks.com/help/stats/t-location-scale-distribution.html
-    %to skoro wyestymowaliœmy parametry, to (X-mu)/sigma jest z rozk³adu
-    %t-Studenta z n stopniami swobody. Czy to ma mi pos³u¿yæ do wyznaczenia
-    %kwantyla? Wtedy ta sigma by³aby tym drugim parametrem wyestymowanym za
-    %pomoc¹ mle?
-    %Zscore = norminv(pVaR,parameters(1),parameters(2));
-    Zscore = tinv(pVaR,parameters(3));
-    %I co z t¹ œredni¹? Czy nie bierzemy jej pod uwagê? (Chodzi mi o wyk³ad
-    %--> wzór na metodê parametryczn¹)
-    T95(i) = Zscore(1)*sigma;
-    T99(i) = Zscore(2)*sigma;
+    Zscore = parameters(2)*tinv(pVaR,parameters(3))+parameters(1);
+    T95(i) = Zscore(1);
+    T99(i) = Zscore(2);
 end
 
 figure;
@@ -43,3 +46,16 @@ xlabel('Data')
 ylabel('VaR')
 legend({'95% poziom ufnoœci ','99% poziom ufnoœci'},'Location','Best')
 title('Estymacja wartoœci zagro¿onej dla metody parametrycznej')
+
+ count95 = 0;
+ count99 = 0;
+ for i=1501:2000
+   if Returns(i) > T95(i-1500)
+       count95 = count95 + 1;
+    end
+    if Returns(i) > T99(i-1500)
+       count99 = count99 + 1;
+    end
+ end
+ (count95/500)*100
+ (count99/500)*100
