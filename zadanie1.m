057e5eedb64096910cd3d1845a0b7859b9a35234
patpaks <wegrzynpatrycja96@gmail.com>
2018-05-21 15:32:04 +0200

    pytania
    pytania

--- zadanie1.m
+++ zadanie1.m
@@ -20,9 +20,17 @@ for t = TestWindow
     i = t - TestWindowStart + 1;
     EstimationWindow = t-EstimationWindowSize:t-1;
     X = Returns(EstimationWindow);
-    sigma = std(Returns(EstimationWindow));
+    sigma = std(X);
     parameters = mle(X,'distribution','tlocationscale');
-    Zscore   = tinv(pVaR,parameters(3));
+    %Tutaj zgodnie z tym co Wojtek pisa³ i tutaj te¿ to jest: http://www.mathworks.com/help/stats/t-location-scale-distribution.html
+    %to skoro wyestymowaliœmy parametry, to (X-mu)/sigma jest z rozk³adu
+    %t-Studenta z n stopniami swobody. Czy to ma mi pos³u¿yæ do wyznaczenia
+    %kwantyla? Wtedy ta sigma by³aby tym drugim parametrem wyestymowanym za
+    %pomoc¹ mle?
+    %Zscore = norminv(pVaR,parameters(1),parameters(2));
+    Zscore = tinv(pVaR,parameters(3));
+    %I co z t¹ œredni¹? Czy nie bierzemy jej pod uwagê? (Chodzi mi o wyk³ad
+    %--> wzór na metodê parametryczn¹)
     T95(i) = Zscore(1)*sigma;
     T99(i) = Zscore(2)*sigma;
 end
