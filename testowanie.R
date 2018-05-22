9b740df7f01da06fb2ac5c5668915d5f60592447
patpaks <wegrzynpatrycja96@gmail.com>
2018-05-22 15:06:43 +0200

    test
    test

--- testowanie.R
+++ testowanie.R
@@ -2,10 +2,12 @@ install.packages('GAS')
 library(GAS)
 
 dane <- read.table(file = "C:/Users/Patrycja/Desktop/ZarzadzanieRyzykiem_zadanie4/dane.txt")
-VaR95 <- dane <- read.table(file = "C:/Users/Patrycja/Desktop/ZarzadzanieRyzykiem_zadanie4/VaR95.txt")
+VaR95 <- read.table(file = "C:/Users/Patrycja/Desktop/ZarzadzanieRyzykiem_zadanie4/VaR95.txt")
 VaR99 <- read.table(file = "C:/Users/Patrycja/Desktop/ZarzadzanieRyzykiem_zadanie4/VaR99.txt")
 
-dane <- as.numeric(unlist(dannn))
-VaR95 <- dane <- as.numeric(unlist(VaR95))
+dane <- as.numeric(unlist(dane))
+VaR95 <- as.numeric(unlist(VaR95))
+VaR99 <- as.numeric(unlist(VaR99))
 
-BacktestVaR(dane, VaR95, 0.05)
+BacktestVaR(-dane, VaR95, 0.95)
+BacktestVaR(-dane, VaR99, 0.01)
